180b576bf49fe4ca714f5596a67342fb5bcb962e
usamagillaniMtechub <usamagillani@mtechub.org>
2024-02-20 18:59:57 +0500

    initial commit after main changes

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, backgroundColor: 'black'}}>
+      <Text style={{color: 'white'}}>App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ app/src/navigation/AuthNavigator/AuthNavigator.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function AuthNavigator() {
+  return (
+    <View>
+      <Text>AuthNavigator</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
